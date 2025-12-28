491348a426efeb30b3884358f01b39624708e901
ramonabrendel1204-sketch <ramona.brendel1204@gmail.com>
2025-12-28 10:50:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Statische Dateien aus dem "public" Ordner servieren
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Alle anderen Anfragen an die index.html leiten (Single Page App Support)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
